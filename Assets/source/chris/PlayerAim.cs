a35c4b24e8b9224519fc3883ac629a4e35c7dc41
Josh Gillum <gill2092@vandals.uidaho.edu>
2025-10-21 19:24:15 -0700

    Got player rotation working

--- Assets/source/chris/PlayerAim.cs
+++ Assets/source/chris/PlayerAim.cs
@@ -27,7 +27,11 @@ public class PlayerAim : MonoBehaviour
         mousePos.z = 0f;
         direction = (mousePos - player.transform.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        player.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        Quaternion rotation = Quaternion.AngleAxis(angle,Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation,rotation,10*Time.deltaTime);
+
+
     }
     private void handleSkill()
     {
