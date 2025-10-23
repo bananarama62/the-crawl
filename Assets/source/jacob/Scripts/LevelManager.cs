e766158a1b5cfaf43dc91331652e08cd1c221eb5
Jacob McKenzie <jamluceononuro@gmail.com>
2025-10-22 22:15:13 -0700

    Added Level Tester Scene with prefab Levels
    Added database of levels with one prefab and a level tester scene

---
+++ Assets/source/jacob/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public RoomDatabase roomDatabase;
+    public Transform levelRoot;
+    private LevelGenerator levelGenerator;
+    public LayerMask roomMask;
+    void Start()
+    {
+        int numRooms = 10;
+        levelGenerator = new LevelGenerator(numRooms, 1, roomMask, levelRoot);
+        levelGenerator.SpawnStart(roomDatabase.rooms[0]);
+        for(int i = 0; i <= numRooms; i++)
+        {
+            levelGenerator.SpawnRoom(roomDatabase.rooms[0]);
+        }
+        
+    }
+
+}
