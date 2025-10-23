a1c53c395961c34c676407efcdd03df67792acda
Csoto96 <csoto550@aol.com>
2025-10-22 20:23:25 -0700

    adding assembly test and chris test

---
+++ Assets/source/Tests/Editmode/ChrisTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class ChrisTest
+{
+    private Mage mage;
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        mage = new Mage();
+    }
+    [Test]
+    public void TestFireBall()
+    {
+        // Use the Assert class to test conditions
+    }
+}
