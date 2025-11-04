f2bc75dd767be4207e3fa085e8ebf897c7c271c7
Josh Gillum <gill2092@vandals.uidaho.edu>
2025-11-03 17:21:52 -0800

    Added timers to chain lighting ability

--- Assets/source/josh/Scripts/usable/effects/chain_lightning.cs
+++ Assets/source/josh/Scripts/usable/effects/chain_lightning.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ChainLightning : Effect {
-  public GameObject[] pylons;
+  private List<GameObject> pylons;
 
   public override int individualEffect(){
     Debug.Log("Casting Chain Lightning...");
     return 1;
+  }
+
+  public void setPylons(List<GameObject> pylons_list){
+    pylons = pylons_list;
   }
 
   void Awake(){
