feb5b45d3ec2a2fb5de1926854121a0be75ac140
Josh Gillum <gill2092@vandals.uidaho.edu>
2025-11-03 20:26:57 -0800

    Animated lightning

--- Assets/source/josh/Scripts/usable/effects/chain_lightning.cs
+++ Assets/source/josh/Scripts/usable/effects/chain_lightning.cs
@@ -26,11 +26,26 @@ public class ChainLightning : Effect {
         current_bolt = Instantiate(LightningBoltPreFab, current_pylon.transform.position, Quaternion.Euler(Vector3.zero));
         Vector3 direction = next_pylon.transform.position - current_pylon.transform.position;
         direction.z = 0;
-        float size = current_bolt.GetComponent<BoxCollider2D>().size.x;
-        float ratio = direction.magnitude / size;
-        current_bolt.transform.localScale = new Vector3(ratio,1f,1f);
+        current_bolt.transform.localScale = new Vector3(3f,3f,1f);
+        current_bolt.GetComponent<Lightning>().setAnimation(0);
+        Vector2 size = current_bolt.GetComponent<BoxCollider2D>().size;
+
+        int count = (int)(direction.magnitude / size.x);
+        float ratio = direction.magnitude / (size.x*count);
+        size = size * ratio;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // Converts Vector3 to an angle
-        current_bolt.transform.rotation = Quaternion.Euler(0f,0f,angle);
+        current_bolt.transform.localScale = new Vector3(ratio,ratio,1f);
+        direction = Vector3.Normalize(direction);
+        Vector3 previous_position = current_bolt.transform.position;
+        Vector3 rotated_length = Vector3.Scale(direction,new Vector3(size.x,size.y,0f));
+        for(int j = 1; j < count; ++j){
+          Debug.Log(direction);
+          Vector3 position = rotated_length + previous_position;
+          current_bolt = Instantiate(LightningBoltPreFab, position, Quaternion.Euler(0f,0f,angle));
+          current_bolt.transform.localScale = new Vector3(3*ratio,3*ratio,1f);
+          current_bolt.GetComponent<Lightning>().setAnimation((j+1)%6);
+          previous_position = current_bolt.transform.position;
+        }
     }
     return 1;
   }
--- Assets/source/josh/Scripts/usable/effects/lightning.cs
+++ Assets/source/josh/Scripts/usable/effects/lightning.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using NUnit.Framework;
-
+using UnityEditor.Animations;
+using System.Collections;
 public class Lightning : MonoBehaviour {
 
   [SerializeField] private float timeToLive; // seconds to live for
   [SerializeField] private int damage;
+  [SerializeField] private AnimatorController animation_loop_1;
+  [SerializeField] private AnimatorController animation_loop_2;
+  [SerializeField] private AnimatorController animation_loop_3;
+  [SerializeField] private AnimatorController animation_loop_4;
+  [SerializeField] private AnimatorController animation_loop_5;
+  [SerializeField] private AnimatorController animation_loop_6;
   private float elapsed_time;
 
   void Start(){
-    Debug.Log("Lightning...");
     elapsed_time = 0;
   }
 
@@ -16,6 +22,27 @@ public class Lightning : MonoBehaviour {
     elapsed_time += Time.deltaTime;
     if(elapsed_time > timeToLive){
       Destroy(gameObject);
+    }
+  }
+
+  public void setAnimation(int num){
+    AnimatorController ChangeTo = null;
+    if(num == 1){
+      ChangeTo = animation_loop_1;
+    } else if(num == 2){
+      ChangeTo = animation_loop_2;
+    } else if(num == 3){
+      ChangeTo = animation_loop_3;
+    } else if(num == 4){
+      ChangeTo = animation_loop_4;
+    } else if(num == 5){
+      ChangeTo = animation_loop_5;
+    } else if(num == 6){
+      ChangeTo = animation_loop_6;
+    }
+
+    if(ChangeTo != null){
+      gameObject.GetComponent<Animator>().runtimeAnimatorController = ChangeTo;
     }
   }
 
