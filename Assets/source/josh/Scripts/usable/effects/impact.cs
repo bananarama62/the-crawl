0f8b5724b088b47472cbf3f0c830a763313fa0c0
Josh Gillum <gill2092@vandals.uidaho.edu>
2025-11-03 16:59:03 -0800

    Spawned pylons have nicer animations and deal impact damage on spawn

---
+++ Assets/source/josh/Scripts/usable/effects/impact.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using NUnit.Framework;
+
+public class Impact : MonoBehaviour {
+
+  private GameObject PlayerLocation;
+  [SerializeField] private float timeToLive; // seconds to live for
+  [SerializeField] private int damage;
+  private float elapsed_time;
+
+  void Start(){
+    Debug.Log("Impact...");
+    elapsed_time = 0;
+  }
+
+  void Update(){
+    elapsed_time += Time.deltaTime;
+    if(elapsed_time > timeToLive){
+      Destroy(gameObject);
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D collision)
+  {
+      PlayerController player = collision.GetComponent<PlayerController>();
+      if (collision.CompareTag("Player"))
+      {
+          player.takeDamage(damage);
+      }
+  }
+
+  void Awake(){
+    PlayerLocation = GameObject.Find("Player");
+    Assert.NotNull(PlayerLocation);
+  }
+}
--- Assets/source/josh/Scripts/usable/effects/spawn_pylon.cs
+++ Assets/source/josh/Scripts/usable/effects/spawn_pylon.cs
@@ -3,13 +3,15 @@ using NUnit.Framework;
 
 public class SpawnPylon : Effect {
   public GameObject PylonPreFab; // Prefab for enemy to spawn
-  [SerializeField] GameObject PlayerLocation;
+  private GameObject PlayerLocation;
   public GameObject SpawnedPylon;
   public override int individualEffect(){
     Debug.Log("Casting Spawn Pylon...");
-    GameObject temp = Instantiate(PylonPreFab, PlayerLocation.transform.position, Quaternion.identity);
+    // Spawn the pylon a bit higher than the player's location so the bottom is in the correct spot since the origin is the top
+    GameObject temp = Instantiate(PylonPreFab, PlayerLocation.transform.position + new Vector3(0,+4,0), Quaternion.identity);
     if(temp){
       SpawnedPylon = temp;
+      SpawnedPylon.GetComponent<FallingObject>().setIndicatorOffset(0,-4);
       return 0;
     }
     return 2;
