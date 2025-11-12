c79c33d483b8f92b607d95c103c28de0919286e0
Josh Gillum <gill2092@vandals.uidaho.edu>
2025-11-11 17:51:27 -0800

    comments

--- Assets/source/josh/Scripts/usable/effects/lightning.cs
+++ Assets/source/josh/Scripts/usable/effects/lightning.cs
@@ -1,8 +1,19 @@
+/*   Author: Josh Gillum              .
+ *   Date: 11 November 2025           ":"         __ __
+ *                                  __|___       \ V /
+ *                                .'      '.      | |
+ *                                |  O       \____/  |
+ *~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~
+ * This file stores the Lightning class, which is spawned by the chain
+ * lightning spell. They have a finite lifespan
+ *~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~^~~
+ */
 using UnityEngine;
 using NUnit.Framework;
 using UnityEditor.Animations;
 using System.Collections;
 public class Lightning : MonoBehaviour {
+   // The six animations for the lightning
    [SerializeField] private AnimatorController animation_loop_1;
    [SerializeField] private AnimatorController animation_loop_2;
    [SerializeField] private AnimatorController animation_loop_3;
@@ -11,14 +22,15 @@ public class Lightning : MonoBehaviour {
    [SerializeField] private AnimatorController animation_loop_6;
    [SerializeField] private float timeToLive; // seconds to live for
    [SerializeField] private int damage;
-   private float elapsed_time;
-   private AnimatorBase AnimationControl = new AnimatorType();
+
+   private AnimatorBase AnimationControl = new AnimatorType(); // Used for dynamic binding
 
    void Start(){
-      Destroy(gameObject,timeToLive);
+      Destroy(gameObject,timeToLive); // Sets timer for when to destroy object
    }
 
    public void setAnimation(int x){
+      // Determines which animation in the chain to use
       AnimatorController ChangeTo = null;
       int num = AnimationControl.getAnimation(x);
       if(num == 1){
@@ -35,13 +47,14 @@ public class Lightning : MonoBehaviour {
          ChangeTo = animation_loop_6;
       }
 
-      if(ChangeTo != null){
+      if(ChangeTo != null){ // If the number was within the bounds of what can set the animation to.
          gameObject.GetComponent<Animator>().runtimeAnimatorController = ChangeTo;
       }
    }
 
    void OnTriggerEnter2D(Collider2D collision)
    {
+      // Only deals damage to the player.
       PlayerController player = collision.GetComponent<PlayerController>();
       if (collision.CompareTag("Player"))
       {
